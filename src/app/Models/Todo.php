2d182c300d77fe3c2a161fc32367be81c24e245c
shige <shigeki.ishihara.0318@gmail.com>
2026-09-24 03:30:09 +0900

    todoアプリ中級を保存

--- src/app/Models/Todo.php
+++ src/app/Models/Todo.php
@@ -7,9 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-  use HasFactory;
+    use HasFactory;
 
-  protected $fillable = [
-    'content'
-  ];
+    protected $fillable = ['category_id', 'content'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function scopeCategorySearch($query, $category_id)
+{
+  if (!empty($category_id)) {
+    $query->where('category_id', $category_id);
+  }
+}
+
+public function scopeKeywordSearch($query, $keyword)
+{
+  if (!empty($keyword)) {
+    $query->where('content', 'like', '%' . $keyword . '%');
+  }
+   return $query;
+}
+
+    
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', [TodoController::class, 'index']);
+Route::get('/todos/search', [TodoController::class, 'search']);
 Route::post('/todos', [TodoController::class, 'store']);
-Route::patch('/todos/update', [TodoController::class, 'update']);
-Route::delete('/todos/delete', [TodoController::class, 'destroy']);
+Route::patch('/todos/{todo}', [TodoController::class, 'update']);
+Route::delete('/todos/{todo}', [TodoController::class, 'destroy']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::patch('/categories/update', [CategoryController::class, 'update']);
+Route::delete('/categories/destroy', [CategoryController::class, 'destroy']);
